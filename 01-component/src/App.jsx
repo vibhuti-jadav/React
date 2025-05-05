d66ad93aec91ff27d4eef0e9a422967ecdeb35f8
vibhuti-jadav <vibhutij109@gmail.com>
2025-05-05 16:45:36 +0530

    component

---
+++ 01-component/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Navbar from './components/navbar.jsx'
+import Card from './components/card.jsx'
+import Footer from './components/footer.jsx'
+import Gallary from './components/Gallary.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+
+      <Card/>
+
+      <Gallary/>
+
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ 01-component/src/components/Gallary.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Gallary = () => {
+  return (
+    <div>
+
+        
+
+<div class="grid gap-4 my-5">
+    
+    <div class="grid grid-cols-5 gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Gallary
