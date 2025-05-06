9a04c29adb56f4db7e101288f63f8a3ee9860af0
vibhuti-jadav <vibhutij109@gmail.com>
2025-05-06 16:40:51 +0530

    props

--- 02-props/src/component/Product.jsx
+++ 02-props/src/component/Product.jsx
@@ -5,9 +5,9 @@ import React from 'react'
  
    return (
      <div>
-         <div className='max-w-7xl mx-auto p-2'>
+         <div className='max-w-7xl mx-auto p-2 my-5'>
  
-             <div className=' grid grid-cols-4 gap-3'>
+             <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
                        {
                            data.map((ele)=>(
                                      <Card key={ele.id} {...ele} />
