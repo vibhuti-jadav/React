50036c2e1294390e73bfc2cec31b055149adbd81
vibhuti-jadav <vibhutij109@gmail.com>
2025-05-19 17:38:40 +0530

    react-component

---
+++ 11-react-component/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import  Toast from './component/Toast'
+import  Offcanvas  from './component/offcanvas'
+
+function App() {
+
+  return (
+    <>
+    <Toast/>
+    <Offcanvas/>
+    </>
+  )
+}
+
+export default App
