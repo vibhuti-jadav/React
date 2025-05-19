80d8701f45c59e47316d4173db9dbc68fd680ee2
vibhuti-jadav <vibhutij109@gmail.com>
2025-05-19 15:56:35 +0530

    UseEffect

--- 05-UseEffect/src/App.jsx
+++ 05-UseEffect/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
-import { RiHeartFill, RiHome2Fill, RiInbox2Line } from '@remixicon/react';
+import { RiArrowDownBoxFill, RiHeartFill, RiHome2Fill, RiInbox2Line } from '@remixicon/react';
 // import Remixicon from './component/remixicon';
 
 
@@ -77,7 +77,7 @@ function App() {
 
 <div className="flex gap-2">
   
-<button onClick={()=>setsort("asc")} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">⬇️</button>
+<button onClick={()=>setsort("asc")} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><RiArrowDownBoxFill/></button>
 
 <button onClick={()=>setsort("desc")} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">⬆️</button>
 </div>
