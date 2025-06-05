b10ade8f88d01f087d2712aa67dfbe6c050713ba
vibhuti-jadav <vibhutij109@gmail.com>
2025-06-05 14:55:25 +0530

    added

--- 18-noteKepper/src/component/todo/NoteInput.jsx
+++ 18-noteKepper/src/component/todo/NoteInput.jsx
@@ -52,8 +52,7 @@ const NoteInput = () => {
                 
                <input 
               //  onChange={(e)=>handleNote(e,index)} 
-              
-               type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+               type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 my-1.5 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
 
               )
             )
